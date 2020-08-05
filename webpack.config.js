4be40cd057f9b7c1d3f5e1c59c4589fdc6528283
conormcp <conormcp@ifa.hawaii.edu>
2020-08-05 11:16:06 -1000

    replace uglify-js with terser-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -11,7 +12,12 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [new CleanWebpackPlugin(), new MiniCssExtractPlugin()],
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
   mode: "production",
+  devtool: "source-map",
   module: {
     rules: [
       {
@@ -23,15 +29,8 @@ module.exports = {
         loader: "file-loader",
         options: {
           outputPath: "images",
-          //  publicPath: "node_modules/visiomatic/lib/images",
         },
       },
-      /*
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        loader: "url-loader",
-      },
-      */
     ],
   },
 };
